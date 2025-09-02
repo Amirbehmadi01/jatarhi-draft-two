174f9f6a5e0c1ea827f76f927eab1a4fda92a769
Amirbehmadi01 <Behmadiamir7@gmail.com>
2025-09-03 02:48:36 +0330

    update responsive

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,28 +1,53 @@
+import img1 from "../assets/images/1.jpg";
+import img2_1 from "../assets/images/2-1.jpg";
+import img2_2 from "../assets/images/2-2.jpg";
+import img3 from "../assets/images/3.jpg";
+import img4 from "../assets/images/4.jpg";
+import img5 from "../assets/images/5.jpg";
+import img6 from "../assets/images/6.jpg";
+import img7 from "../assets/images/7.gif";
+import img8 from "../assets/images/8.jpg";
+import img9_1 from "../assets/images/9-1.jpg";
+import img9_2 from "../assets/images/9-2.jpg";
+import img9_3 from "../assets/images/9-3.jpg";
+import img10 from "../assets/images/10.jpg";
+import img11 from "../assets/images/11.jpg";
+import img12 from "../assets/images/12.jpg";
+import img13 from "../assets/images/13.jpg";
+import img14 from "../assets/images/14.jpg";
+import img15 from "../assets/images/15.jpg";
+import img16 from "../assets/images/16.jpg";
+import img17 from "../assets/images/17.jpg";
+import img18 from "../assets/images/18.jpg";
+import img19 from "../assets/images/19.jpg";
+import img20 from "../assets/images/20.jpg";
+import img21 from "../assets/images/21.jpg";
+
 const portfolioData = [
-    { src: "../public/images/1.jpg", description: "نمونه کار 1" },
-    { src: "../public/images/2-1.jpg", description: "نمونه کار 2" },
-    { src: "../public/images/2-2.jpg", description: "نمونه کار 3" },
-    { src: "../public/images/3.jpg", description: "نمونه کار 4" },
-    { src: "../public/images/4.jpg", description: "نمونه کار 5" },
-    { src: "../public/images/5.jpg", description: "نمونه کار 6" },
-    { src: "../public/images/6.jpg", description: "نمونه کار 7" },
-    { src: "../public/images/7.gif", description: "نمونه کار 8" },
-    { src: "../public/images/8.jpg", description: "نمونه کار 9" },
-    { src: "../public/images/9-1.jpg", description: "نمونه کار 10" },
-    { src: "../public/images/9-2.jpg", description: "نمونه کار 11" },
-    { src: "../public/images/9-3.jpg", description: "نمونه کار 12" },
-    { src: "../public/images/10.jpg", description: "نمونه کار 13" },
-    { src: "../public/images/11.jpg", description: "نمونه کار 14" },
-    { src: "../public/images/12.jpg", description: "نمونه کار 15" },
-    { src: "../public/images/13.jpg", description: "نمونه کار 16" },
-    { src: "../public/images/14.jpg", description: "نمونه کار 17" },
-    { src: "../public/images/15.jpg", description: "نمونه کار 18" },
-    { src: "../public/images/16.jpg", description: "نمونه کار 19" },
-    { src: "../public/images/17.jpg", description: "نمونه کار 20" },
-    { src: "../public/images/18.jpg", description: "نمونه کار 21" },   
-    { src: "../public/images/19.jpg", description: "نمونه کار 22" },
-    { src: "../public/images/20.jpg", description: "نمونه کار 23" },
-    { src: "../public/images/21.jpg", description: "نمونه کار 24" }
-  ];
-  
-  export default portfolioData;
+  { src: img1, description: "نمونه کار 1" },
+  { src: img2_1, description: "نمونه کار 2" },
+  { src: img2_2, description: "نمونه کار 3" },
+  { src: img3, description: "نمونه کار 4" },
+  { src: img4, description: "نمونه کار 5" },
+  { src: img5, description: "نمونه کار 6" },
+  { src: img6, description: "نمونه کار 7" },
+  { src: img7, description: "نمونه کار 8" },
+  { src: img8, description: "نمونه کار 9" },
+  { src: img9_1, description: "نمونه کار 10" },
+  { src: img9_2, description: "نمونه کار 11" },
+  { src: img9_3, description: "نمونه کار 12" },
+  { src: img10, description: "نمونه کار 13" },
+  { src: img11, description: "نمونه کار 14" },
+  { src: img12, description: "نمونه کار 15" },
+  { src: img13, description: "نمونه کار 16" },
+  { src: img14, description: "نمونه کار 17" },
+  { src: img15, description: "نمونه کار 18" },
+  { src: img16, description: "نمونه کار 19" },
+  { src: img17, description: "نمونه کار 20" },
+  { src: img18, description: "نمونه کار 21" },   
+  { src: img19, description: "نمونه کار 22" },
+  { src: img20, description: "نمونه کار 23" },
+  { src: img21, description: "نمونه کار 24" }
+];
+
+export default portfolioData;
