044786b1b8c160003424a008292647df82c777d4
Amirbehmadi01 <Behmadiamir7@gmail.com>
2025-09-12 13:37:14 +0330

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/header.css";
+import webbaner from "../assets/headerPic/webbanner.gif"
 
 export default function Header() {
   return (
     <header className="header">
-      <img src="/images/header.jpg" alt="Header" className="header-img" />
+    <img src={webbaner} alt="Header" className="header-img" />
     </header>
   );
 }
